eaea997f6e9a11e18d1c9dd460b2b555aa5d2e1a
VadCore <vadim.azure@gmail.com>
2021-07-27 17:08:40 +0300

    upd

---
+++ LinqLearn/Services/FilterCombinerExtension.cs
@@ -0,0 +1,25 @@
+﻿using LinqLearn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace LinqLearn.Services
+{
+    public static class FilterCombinerExtension
+    {
+        public static Expression<Func<T, bool>> And<T>(
+            this Expression<Func<T, bool>> left,
+            Expression<Func<T, bool>> right)
+        {
+
+        return Expression.Lambda<Func<T, bool>>(
+            Expression.AndAlso(
+                left.Body,
+                Expression.Invoke(right, left.Parameters)
+            ),
+            left.Parameters);
+        }
+    }
+}
--- LinqLearn/Services/GameFilterProvider.cs
+++ LinqLearn/Services/GameFilterProvider.cs
@@ -1,18 +1,49 @@
 ﻿using System;
 using System.Linq.Expressions;
 using LinqLearn.Models;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace LinqLearn.Services
 {
     public class GameFilterProvider : IGameFilterProvider
     {
-        public Func<Game, bool> GetFilterFunction(GameSearchSettings searchSettingsCollection)
+
+        
+
+        public Func<Game, bool> GetFilterFunction(GameSearchSettings filter)
         {
-            Expression<Func<Game, bool>> filterExpression = game => true;
+            //var minPrice = filter.MinPrice ?? 0;
+            //var maxPrice = (filter.MaxPrice is null || filter.MaxPrice == 0)  ? decimal.MaxValue : filter.MaxPrice;
+            //var genresCount = filter.Genres.Count();
 
-            //TODO: implement logic here;
+            //Expression<Func<Game, bool>> predicate = game =>
+            //(string.IsNullOrEmpty(filter.Name) || game.Name == filter.Name)
+            //&&
+            //game.Price <= maxPrice && game.Price >= minPrice
+            //&&
+            //(genresCount == 0 || game.Genres.Intersect(filter.Genres).Count() == genresCount);
 
-            return filterExpression.Compile();
+            //return predicate.Compile();
+
+            Expression<Func<Game, bool>> predicate = game => true;
+
+            if (!string.IsNullOrEmpty(filter.Name))
+                predicate = predicate.And(g => g.Name == filter.Name);
+
+            if(filter.MinPrice != null && filter.MinPrice != 0)
+                predicate = predicate.And(g => g.Price >= filter.MinPrice);
+
+            if (filter.MaxPrice != null && filter.MaxPrice != 0)
+                predicate = predicate.And(g => g.Price <= filter.MaxPrice);
+
+            if (filter.Genres.Count > 0)
+                predicate = predicate.And(g => g.Genres.Intersect(filter.Genres).Count() == filter.Genres.Count);
+
+
+
+            return predicate.Compile();
         }
     }
 }
